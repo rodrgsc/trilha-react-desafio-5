73e5c1d3a1fdaea6a61406bca89ed779c6512b1d
rodrgsc <rodrgcs2@gmail.com>
2024-09-18 19:37:13 -0300

    api config

--- services/api.js
+++ services/api.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
-    }
-})
+// export const api  = axios.create({
+//     baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+//     headers: {
+//         apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
+//         authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+//     }
+// })
+export const api = axios.create({
+  baseURL: 'https://qjkcqnmwtedfzcubnlvj.supabase.co/rest/v1',
+  headers: {
+    apikey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqa2Nxbm13dGVkZnpjdWJubHZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2ODA4MjMsImV4cCI6MjA0MjI1NjgyM30.1BkHBa0AMcMf56MB9vGKfUeFCqKjFwIgL05E-vUb0jA',
+    authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqa2Nxbm13dGVkZnpjdWJubHZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2ODA4MjMsImV4cCI6MjA0MjI1NjgyM30.1BkHBa0AMcMf56MB9vGKfUeFCqKjFwIgL05E-vUb0jA',
+  },
+});
