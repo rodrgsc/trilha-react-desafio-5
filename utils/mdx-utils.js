7a7314ae500c85e49d3b80fb86014fd5525c5f47
rodrgsc <rodrgcs2@gmail.com>
2024-09-18 17:42:36 -0300

    Buscar um post

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,19 +1,30 @@
-import { api } from '../services/api'
+import { api } from '../services/api';
 
 export const getPosts = async () => {
-    const {data} = await api.get('/posts'); 
+  const { data } = await api.get('/posts');
 
-    if(data){
-        return data;
-    }
+  if (data) {
+    return data;
+  }
 
-    return []
-}
+  return [];
+};
 
 export const getPostBySlug = async (id) => {
+  //TODO: BUSCAR UM POST EM ESPECIFICO.
+  //   const { data } = await api.get(`/post?id=eq.${id}`);
+  try {
+    const { data } = await api.get(`/post?id=eq.${id}`);
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
-
-    return {}
-}
+    // Verifica se há dados retornados
+    if (data && data.length > 0) {
+      console.log(data);
+      return data[0];
+    } else {
+      return {};
+    }
+  } catch (error) {
+    console.error('Erro ao buscar o post:', error);
+    return {};
+  }
+};
